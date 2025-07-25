721cc57ddec8a11c79ca6f62c2e74e01fd79bda7
alxspiker <alxtheprogrammer@gmail.com>
2025-07-24 19:17:54 -0600

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,18 @@
 
 #include <stdio.h>
 #include <stdint.h> // Required for uint32_t, etc.
+#ifdef _WIN32
+#ifndef APIENTRY
+#define APIENTRY __stdcall
+#endif
+#ifndef WINGDIAPI
+#define WINGDIAPI __declspec(dllimport)
+#endif
+#ifndef GLAPI
+#define GLAPI WINGDIAPI
+#endif
+#endif
+
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <SDL2/SDL.h>
